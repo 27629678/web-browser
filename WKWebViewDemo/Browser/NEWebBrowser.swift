a4e299e13fae5dc24377249b6a257cc5f6b04044
yuxiaohua <hzyuxiaohua@corp.netease.com>
2018-01-09 09:51:02 +0800

    add wkwebview

---
+++ WKWebViewDemo/Browser/NEWebBrowser.swift
@@ -0,0 +1,15 @@
+//
+//  NEWebBrowser.swift
+//  WKWebViewDemo
+//
+//  Created by hzyuxiaohua on 09/01/2018.
+//  Copyright © 2018 hzyuxiaohua. All rights reserved.
+//
+
+import WebKit
+
+class NEWebBrowser: WKWebView {
+
+    
+
+}
